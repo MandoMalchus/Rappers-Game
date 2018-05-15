73c99b33bf2c0e0d8f3f39b6b61cef6164b8b785
Armando Vargas <Mando@Armandos-MacBook-Pro.local>
2018-05-15 13:43:57 -0400

    rapper commit

---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,11 @@
+
+import React from "react";
+import "./Jumbotron.css";
+
+const Jumbotron = props => (
+    <div className="jumbotron">
+        <h1>Click on the hottest RAPPERS in the GAME to begin.
+        Click on an image to earn points, but don't click anymore than once!</h1>
+    </div>
+);
+export default Jumbotron;
